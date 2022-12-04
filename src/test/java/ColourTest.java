9646647b1d219a04759badcc7e190b96b2f33539
darragh <120389806@umail.ucc.ie>
2022-12-04 20:25:44 +0000

    Created createColourWithFloatValues test - test will fail

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -1,3 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class ColourTest {
+
+    @Test
+    public void createColourWithFloatValues() {
+        Colour testColour = new Colour(0.5f, 0.3f, 0.6f);
+        assert testColour != null;
+    }
 }
