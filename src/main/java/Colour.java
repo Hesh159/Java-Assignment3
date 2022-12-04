b3dc4a034744e9cf971a57acbd7256ec3e371b3a
darragh <120389806@umail.ucc.ie>
2022-12-04 20:04:45 +0000

    Created maven build file and Colour and ColourTest classes

---
+++ src/main/java/Colour.java
@@ -0,0 +1,12 @@
+
+public class Colour {
+
+    //colours represented as integers in range 0 - 255
+    public int red;
+    public int green;
+    public int blue;
+
+    public Colour(float r, float g, float b) {
+
+    }
+}
