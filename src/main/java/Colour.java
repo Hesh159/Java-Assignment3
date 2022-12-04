4c5799ee170df4f45737f622404aa0c130a74c6d
darragh <120389806@umail.ucc.ie>
2022-12-04 22:37:18 +0000

    Edited Colour integer constructor to throw an IllegalArgumentException if the input is too large (larger than 24 bits)

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -16,6 +16,10 @@ public class Colour {
 
 
     public Colour(int rgbValue) {
+        if (rgbValue > 16777215) {
+            System.out.println("Error: Integer Value must be below 16,777,215");
+            throw new IllegalArgumentException();
+        }
         redValue = (rgbValue >> 16) & 255;
         greenValue = (rgbValue >> 8) & 255;
         blueValue = rgbValue & 255;
