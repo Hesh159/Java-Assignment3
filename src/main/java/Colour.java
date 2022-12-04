3a666f23ad47ad86a18a672f1535e5cb7fffcdae
darragh <120389806@umail.ucc.ie>
2022-12-04 20:27:22 +0000

    Created Colour constructor that takes 3 floats, changed variables for rgb to private

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -2,8 +2,11 @@
 public class Colour {
 
     //colours represented as integers in range 0 - 255
-    public int red;
-    public int green;
-    public int blue;
+    private int redValue;
+    private int greenValue;
+    private int blueValue;
 
+    public Colour(float r, float g, float b) {
+
+    }
 }
